2b7b6a5cccf384fb3b43ea852c29768c51dd6459
LGuyver <lguyver@hotmail.co.uk>
2018-11-30 10:32:03 +0000

    Resetting the quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,6 +5,10 @@ import QuizEnd from "./QuizEnd.js";
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
+  handleResetClick() {
+    this.setState({ quiz_position: 1 });
+  }
+
   constructor(props) {
     super(props);
     this.state = { quiz_position: 1 };
@@ -22,7 +26,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHanlder={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             quiz_question={
